331557121446840c09b4f2e310e8dccdeadb1673
Rukmi Patel <rukmi.patel@itreeni.com>
2017-01-09 15:51:54 +0530

    - Basic Laravel Set up with authentication  - HTML for phase 1

---
+++ design/phase-1/HTML/js/dashboard.js
@@ -0,0 +1,34 @@
+(function($){
+  
+  $(function(){
+
+    $(window).scroll(function (event) {
+        var scroll = $(window).scrollTop();
+        if(scroll >= 100)
+        {
+            $(".nav-bar").removeClass("blue-grey darken-4");
+            //$(".nav-bar").addClass("deep-orange darken-3");
+        }
+        else
+        {
+            //$(".nav-bar").removeClass("deep-orange darken-3");
+            $(".nav-bar").addClass("blue-grey darken-4");
+        }
+    });
+
+    $(".game-banner").hover(
+        function() {
+            $( this ).stop().animate({
+                height: "200px"
+            },500);
+        }, function() {
+             $( this ).stop().animate({
+                height: "130px"
+            },500);
+        }
+    );
+
+  }); // end of document ready
+
+
+})(jQuery); // end of jQuery name space
---
+++ design/phase-1/HTML/js/init.js
@@ -0,0 +1,60 @@
+(function($){
+  
+  
+  $(function(){
+    //initialize sidebar pushpin
+    //$('.left-side-nav').pushpin({ top: $('.main-container').offset().top,offset: $(".nav-bar").height()+20 });
+    $(".left-side-nav").height($(".main-container").height());
+    onElementHeightChange($(".main-content")[0], function(){
+        $(".left-side-nav").height($(".main-container").height());
+    });
+    //initialize modal triggers
+    $('.modal-trigger').leanModal({
+      opacity: .7, // Opacity of modal background
+    });
+
+    $(".team-name").on("click",function(){
+        var gameid = $(this).data("gameid");
+        if($(".game-players-"+gameid).is(":hidden"))
+        {
+            $(".game-players-"+gameid).fadeIn(600);
+        }
+        else
+        {
+            $(".game-players-"+gameid).hide();
+        }
+        
+    });
+
+
+    $("#signupBtn").on("click",function(data){
+        $('#loginModal').closeModal();
+        $('#signUpModal').openModal();
+
+    });
+
+    $("#loginBtn").on("click",function(data){
+        $('#signUpModal').closeModal();
+        $('#loginModal').openModal();
+    });
+
+
+  }); // end of document ready
+
+
+})(jQuery); // end of jQuery name space
+
+function onElementHeightChange(elm, callback){
+    var lastHeight = elm.clientHeight, newHeight;
+    (function run(){
+        newHeight = elm.clientHeight;
+        if( lastHeight != newHeight )
+            callback();
+        lastHeight = newHeight;
+
+        if( elm.onElementHeightChangeTimer )
+            clearTimeout(elm.onElementHeightChangeTimer);
+
+        elm.onElementHeightChangeTimer = setTimeout(run, 200);
+    })();
+}
