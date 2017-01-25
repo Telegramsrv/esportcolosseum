e622a207e98822b80b2fd53fe75fe108c4f392e0
Rahul Dhokia <rahul.dhokia@itreeni.com>
2017-01-25 15:14:57 +0530

    Chat server host debug

--- code/phase-1/resources/views/admin/match/chat.blade.php
+++ code/phase-1/resources/views/admin/match/chat.blade.php
@@ -30,7 +30,7 @@
 @section('footer') 
 <script>
     //The homestead or local host server (don't forget the ws prefix)
-    var host = 'ws://<?php echo env('DB_HOST');?>:8889';
+    var host = 'ws://127.0.0.1:8889';
     var socket = null;
     var input = document.getElementById('input');
     var messages = document.getElementById('messages');
