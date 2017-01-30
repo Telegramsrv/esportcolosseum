38fd2f8daec473619ac58b4eb275fb97f2b74c29
Rukmi Patel <rukmi.patel@itreeni.com>
2017-01-30 16:16:02 +0530

    remove camel case problem

--- code/phase-1/app/Http/Controllers/User/ChallengeController.php
+++ code/phase-1/app/Http/Controllers/User/ChallengeController.php
@@ -3,7 +3,7 @@
 namespace App\Http\Controllers\User;
 
 use Illuminate\Http\Request;
-use App\Http\Requests\Challenge\createOpenChallengeRequest;
+use App\Http\Requests\Challenge\CreateOpenChallengeRequest;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use App\Models\Game;
