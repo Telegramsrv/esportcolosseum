d6ccd59a6e0155e149e9ff53c92dfb8316ca861a
Rahul Dhokia <rahul.dhokia@itreeni.com>
2017-01-31 16:29:03 +0530

    Added coin limit rules in text box

--- code/phase-1/resources/views/layouts/user/partials/add-coin.blade.php
+++ code/phase-1/resources/views/layouts/user/partials/add-coin.blade.php
@@ -5,7 +5,7 @@
 	      	<div id="amountForm" class="modal-form-container">
 	            <div class="row">
 	        		<div class="input-field col s12 m12">
-	        			{!! Form::text('coins', '', ['class' => 'validate input-field col s9 m9', 'id' => 'coins', 'name' => 'coins']) !!}
+	        			{!! Form::text('coins', '', ['class' => 'validate input-field col s9 m9', 'id' => 'coins', 'name' => 'coins', 'maxlength' => 8]) !!}
 	        			{!! Form::label('Coins', 'Coins', ['data-error' => '', 'id' => 'amountLabel']) !!}
 	        			<span class="input-field col s3 m3" id="coinMoney">&nbsp;</span>
 	        			<span class="input-field col s12 m12">{{ $options->coins_per_dollar }} coins = 1$</span>
