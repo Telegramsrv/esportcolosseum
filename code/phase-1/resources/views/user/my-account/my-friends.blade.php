5862a7cb9c0ee08e621984620b0d27c4fd186bae
Rahul Dhokia <rahul.dhokia@itreeni.com>
2017-02-08 18:27:27 +0530

    Notifications

--- code/phase-1/resources/views/user/my-account/my-friends.blade.php
+++ code/phase-1/resources/views/user/my-account/my-friends.blade.php
@@ -2,30 +2,25 @@
 @section('static-content')
 	<div class="tab-content col s12 m12">
 		<div class="section-title">My <span>Friends</span></div>
-		<div class="row">
-		@if(count($userFriends) > 0)
-			<table class="responsive-table striped highlight">
-		        <thead>
-		          <tr>
-		              <th data-field="id">First Name</th>
-		              <th data-field="name">Last Name</th>
-		              <th data-field="price">Image</th>
-		          </tr>
-		        </thead>
-			
-		        <tbody>
-		        @foreach ($userFriends as $key => $userFriend)
-		        	<tr>
-						<td>{{$userFriend->userFriendDetails->first_name}}</td>
-						<td>{{ $userFriend->userFriendDetails->last_name }}</td>
-						<td><img class="user-image" src="{{ url(env('PROFILE_PICTURE_PATH').$userFriend->userFriendDetails->user_image) }}"></td>
-					</tr>
-		        @endforeach
-		        </tbody>
-		     </table>
-		    @else
+		<?php $cnt = count($userFriends);?>
+		@if($cnt > 0)
+			@foreach ($userFriends as $key => $userFriend)
+				@if($key % 2 == 0)
+				@if($key != 0) 
+				</div>
+				@endif
+				<div class="row">
+				@endif
+			      <div class="col s6">
+			      		<img class="user-image" src="{{ url(env('PROFILE_PICTURE_PATH').$userFriend->userFriendDetails->user_image) }}">
+			      		{{$userFriend->userFriendDetails->first_name}} {{ $userFriend->userFriendDetails->last_name }}
+			      </div>
+			     @if($key == ($cnt-1))
+			    </div>
+			    @endif
+			 @endforeach
+    	@else
 		     No Friend Found
-		    @endif
-		</div>
+		@endif
 	</div>	
 @endsection
