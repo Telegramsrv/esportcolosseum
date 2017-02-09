5b3de37ec15269dd81b58bc36601b9087dcf24f4
Rahul Dhokia <rahul.dhokia@itreeni.com>
2017-02-09 15:41:42 +0530

    My friends page css

--- code/phase-1/resources/views/user/my-account/my-friends.blade.php
+++ code/phase-1/resources/views/user/my-account/my-friends.blade.php
@@ -13,7 +13,7 @@
 				<div class="row">
 				@endif
 			      <div class="col s4 text-center">
-			      		<a href="{{ url('user/profile/'.md5($userFriend->user_id)) }}"><img class="friend-img responsive-img circle" src="{{ url(env('PROFILE_PICTURE_PATH').$userFriend->user_image) }}"></a>
+			      		<a href="{{ url('user/profile/'.md5($userFriend->user_id)) }}"><img class="friend-img responsive-img circle" src="{{ url(env('PROFILE_PICTURE_PATH').($userFriend->user_image != '' ? $userFriend->user_image : env('DEFAULT_USER_PROFILE_IMAGE'))) }}"></a>
 			      		<br>
 			      		<b>{{$userFriend->first_name}} {{ $userFriend->last_name }}</b>
 			      </div>
