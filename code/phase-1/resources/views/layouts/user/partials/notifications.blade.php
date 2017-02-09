e985d743c94d076109469d5e0d15ec79884a5776
Rahul Dhokia <rahul.dhokia@itreeni.com>
2017-02-09 18:23:24 +0530

    Notification changes

--- code/phase-1/resources/views/layouts/user/partials/notifications.blade.php
+++ code/phase-1/resources/views/layouts/user/partials/notifications.blade.php
@@ -1,14 +1,18 @@
 <ul id="dropdown2" class="dropdown-content">
 	<?php $total = count($notifications); ?>
-	@foreach($notifications as $notification)
-		<?php $notification->data = json_decode($notification->data); ?>
-		<li id="notification-{{ $notification->id }}">
-			<a href="#!">
-				{!!  $notification->message !!}
-				<i class="fa fa-check-circle fa-lg" aria-hidden="true" onClick="acceptFriend({{ $notification->data->from_id }}, {{ $notification->id }})"></i>
-				<i class="fa fa-times fa-lg" aria-hidden="true" onClick="rejectFriend({{ $notification->data->from_id }}, {{ $notification->id }})"></i>
-			</a>
-		</li>
-	@endforeach
+	@if($total > 0)
+		@foreach($notifications as $notification)
+			<?php $notification->data = json_decode($notification->data); ?>
+			<li id="notification-{{ $notification->id }}">
+				<a href="#!">
+					{!!  $notification->message !!}
+					<i class="fa fa-check-circle fa-lg" aria-hidden="true" onClick="acceptFriend({{ $notification->data->from_id }}, {{ $notification->id }})"></i>
+					<i class="fa fa-times fa-lg" aria-hidden="true" onClick="rejectFriend({{ $notification->data->from_id }}, {{ $notification->id }})"></i>
+				</a>
+			</li>
+		@endforeach
+	@else
+		<li><a href="#!">Notification not found.</a></li>
+	@endif
 </ul>
-<a class="dropdown-button white-text" href="#!" data-activates="dropdown2"><span id="total-notification" class="badge white-text">{{$total}}</span><i class="fa fa-bell-o collection-item" aria-hidden="true"></i></a>
+<a class="dropdown-button white-text" href="#!" data-activates="dropdown2"><span id="total-notification" class="badge white-text">{{$total > 0 ? $total : ''}}</span><i class="fa fa-bell-o collection-item" aria-hidden="true"></i></a>
