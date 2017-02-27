0fab7ab8a52e9638500871b204ae0a4e0e0f76be
Rukmi Patel <rukmi.patel@itreeni.com>
2017-02-27 13:17:10 +0530

    Match history changes

--- code/phase-1/resources/views/user/partials/challenge/match-history.blade.php
+++ code/phase-1/resources/views/user/partials/challenge/match-history.blade.php
@@ -23,7 +23,8 @@
                     $result = "";
                     if($match->game_type == "solo")  {
                         $result = ($match->winner_id == Auth::id()) ? "Won": "Loss";
-                        $opponent = ($match->user_id == Auth::id()) ? $match->opponent_id : $match->user_id;
+                        $opponentUser = ($match->user_id == Auth::id()) ? $match->opponent : $match->captain;
+                        $opponent = $opponentUser->userDetails->first_name . " " . $opponentUser->userDetails->last_name;
                     } else if($match->game_type == "team") {
                         $winnerTeam =  $match->winnerTeam();
                         $myChallengeTeams = myChallengeTeams($match, Auth::id());
