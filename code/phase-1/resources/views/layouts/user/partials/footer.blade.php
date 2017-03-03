3f5497c4c9ea8ca3c49b63c7d3c02638b85f62bc
Rahul Dhokia <rahul.dhokia@itreeni.com>
2017-03-03 17:07:15 +0530

    Footer pages link

--- code/phase-1/resources/views/layouts/user/partials/footer.blade.php
+++ code/phase-1/resources/views/layouts/user/partials/footer.blade.php
@@ -4,14 +4,14 @@
             <div class="col l9 s12">
                 <h5 class="white-text">Site Links</h5>
                 <div class="row footer-row">
-                    <div class="link-row"><a  href="#!">Home</a></div>
-                    <div class="link-row"><a  href="#!">About</a></div>
-                    <div class="link-row"><a  href="#!">FAQ</a></div>
-                    <div class="link-row"><a  href="#!">Press</a></div>
+                    <div class="link-row"><a  href="/">Home</a></div>
+                    <div class="link-row"><a  href="/page/about-us">About</a></div>
+                    <div class="link-row"><a  href="/page/faq">FAQ</a></div>
+                    <div class="link-row"><a  href="/page/press">Press</a></div>
                     <div class="link-row"><a  href="#!">Contact</a></div>
                     <div class="link-row"><a  href="#!">Careers</a></div>
-                    <div class="link-row"><a  href="#!">Privacy</a></div>
-                    <div class="link-row"><a  href="#!">Legal</a></div>
+                    <div class="link-row"><a  href="/page/privacy">Privacy</a></div>
+                    <div class="link-row"><a  href="/page/legal">Legal</a></div>
                 </div>
             </div>
             <div class="col l3 s12">
